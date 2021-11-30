62f3eb9dda53958469c140c81d783f673680b451
Neyuux <pixatoad@gmail.com>
2021-11-30 10:55:32 +0100

    CustomItemStack & ConfigItem

--- src/fr/neyuux/refont/lg/config/ConfigItem.java
+++ src/fr/neyuux/refont/lg/config/ConfigItem.java
@@ -1,6 +1,53 @@
 package fr.neyuux.refont.lg.config;
 
 import fr.neyuux.refont.lg.utils.CustomItemStack;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 
 public class ConfigItem extends CustomItemStack {
+
+    public ConfigItem(Material material, String name) {
+        super(material, name);
+    }
+
+    public ConfigItem(Material material, String name, String... lore) {
+        super(material, name, lore);
+    }
+
+    public ConfigItem(Material material, int amount, String name) {
+        super(material, amount, name);
+    }
+
+    public ConfigItem(Material material, int amount, String name, String... lore) {
+        super(material, amount, name, lore);
+    }
+
+    public ConfigItem(Material material, int amount, short durabilite, String name) {
+        super(material, amount, durabilite, name);
+    }
+
+    public ConfigItem(Material material, short durabilite, String name) {
+        super(material, durabilite, name);
+    }
+
+    public ConfigItem(Material material, short durabilite) {
+        super(material, durabilite);
+    }
+
+    public ConfigItem(Material material, short durabilite, String name, String... lore) {
+        super(material, durabilite, name, lore);
+    }
+
+    public ConfigItem(Material material) {
+        super(material);
+    }
+
+    public ConfigItem(ItemStack item) {
+        super(item);
+    }
+
+
+    public void onClick() {
+
+    }
 }
